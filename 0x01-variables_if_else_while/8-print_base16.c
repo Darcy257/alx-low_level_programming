f72a2dead348bfbce0442615d8f467166dead54d
Darcy <darcy>
2023-03-18 12:11:00 -0700

    print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print base 16 numbers in lowercases
+ *
+ * Return: always 0 for success
+ */
+
+int main(void)
+{
+int i;
+char j;
+for (i = 0 ; i < 10 ; i++)
+	putchar(i + '0');
+		for (j = 'a' ; j <= 'f' ; j++)
+			putchar(j);
+		return (0);
+}
