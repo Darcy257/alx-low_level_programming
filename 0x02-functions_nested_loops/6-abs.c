083bf54a566c4677b4a8de097829dcd2d6949486
Darcy <darcy>
2023-03-22 14:37:25 -0700

    absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - print aabsolute values
+ *
+ * @a: parameter
+ *
+ * Return: always a
+ */
+
+int _abs(int a)
+{
+if (a >= 0)
+{
+	return (a);
+}
+else
+{
+return (a * -1);
+}
+}
