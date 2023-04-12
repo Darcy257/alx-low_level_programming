b8ee01d402ee2c417f77d6ee569aab478f68c975
Darcy <darcy>
2023-04-11 23:48:02 -0700

    0-create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,8 @@ unsigned int i;
 
 str = malloc(sizeof(char) * size);
 if (size == 0 || str == NULL)
-	for (i = 0 ; i < size ; i++)
-		str[i] = c;
+return (NULL);
+for (i = 0 ; i < size ; i++)
+str[i] = c;
 return (str);
 }
